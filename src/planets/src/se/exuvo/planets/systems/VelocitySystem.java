9424ff1b2307900b8eb98f16edc76973b108cf6b
exuvo <exuvo@exuvo.se>
2013-04-24 18:47:21 +0200

    Planets now stay inside screen.

--- src/planets/src/se/exuvo/planets/systems/VelocitySystem.java
+++ src/planets/src/se/exuvo/planets/systems/VelocitySystem.java
@@ -9,13 +9,20 @@ import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.annotations.Mapper;
 import com.artemis.systems.IntervalEntityProcessingSystem;
+import com.badlogic.gdx.Gdx;
 
 public class VelocitySystem extends IntervalEntityProcessingSystem {
 	@Mapper	ComponentMapper<Velocity> vm;
 	@Mapper	ComponentMapper<Position> pm;
 
+	private float maxX, maxY, minX, minY;
+	
 	public VelocitySystem() {
 		super(Aspect.getAspectForAll(Velocity.class, Position.class), Settings.getFloat("PhysicsStep"));
+		maxX = Gdx.graphics.getWidth()/2;
+		maxY = Gdx.graphics.getHeight()/2;
+		minX = -maxX;
+		minY = -maxY;
 	}
 
 	@Override
@@ -25,6 +32,11 @@ public class VelocitySystem extends IntervalEntityProcessingSystem {
 		
 		// apply speed to position
 		p.vec.add(v.vec);
+		
+		if(p.vec.x < minX) p.vec.x = minX;
+		if(p.vec.y < minY) p.vec.y = minY;
+		if(p.vec.x > maxX) p.vec.x = maxX;
+		if(p.vec.y > maxY) p.vec.x = maxY;
 	}
 
 }
