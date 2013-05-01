f75c166f8083d0291b48a60a86494b38755fc61e
exuvo <exuvo@exuvo.se>
2013-05-01 22:27:28 +0200

    Sometimes I hate eclipse.

---
+++ src/planets/src/se/exuvo/planets/components/Particle.java
@@ -0,0 +1,24 @@
+package se.exuvo.planets.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+/**
+ * Holds a particle effect. Usually for a planet collision.
+ */
+public class Particle extends Component {
+	public ParticleEffect effect;
+
+	public Particle() {
+		this(new ParticleEffect());
+		TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal("resources/particles.atlas"));
+		effect.load(Gdx.files.internal("resources/src/particles/test.p"), textureAtlas);
+		effect.start();
+	}
+
+	public Particle(ParticleEffect initialEffect) {
+		effect = initialEffect;
+	}
+}
---
+++ src/planets/src/se/exuvo/planets/systems/ParticleSystem.java
@@ -0,0 +1,55 @@
+package se.exuvo.planets.systems;
+
+import se.exuvo.planets.components.Particle;
+import se.exuvo.planets.components.Position;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.annotations.Mapper;
+import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ParticleSystem extends EntityProcessingSystem {
+	@Mapper	ComponentMapper<Particle> pam;
+	@Mapper	ComponentMapper<Position> pm;
+	
+	private SpriteBatch spriteBatch;
+	private OrthographicCamera camera;
+
+	public ParticleSystem(OrthographicCamera camera) {
+		super(Aspect.getAspectForAll(Particle.class, Position.class));
+		this.camera = camera;
+	}
+	
+	@Override
+	protected void initialize() {
+		spriteBatch = new SpriteBatch();
+	}
+
+	@Override
+	protected void begin() {
+		spriteBatch.setProjectionMatrix(camera.combined);
+		spriteBatch.begin();
+	}
+	
+	@Override
+	protected void process(Entity e) {
+		Position p = pm.get(e);
+		ParticleEffect effect = pam.get(e).effect;
+		effect.setPosition(p.vec.x, p.vec.y);
+		effect.draw(spriteBatch, Gdx.graphics.getDeltaTime());
+		if(effect.isComplete()){
+			e.deleteFromWorld();
+		}
+	}
+	
+	@Override
+	protected void end() {
+		spriteBatch.end();
+	}
+	
+}
