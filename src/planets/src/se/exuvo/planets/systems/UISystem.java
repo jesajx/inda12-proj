a24abddbec6842db8ef7aa03888f3d1a2ecc9553
exuvo <exuvo@exuvo.se>
2013-05-01 14:11:57 +0200

    UI window with basic fields.

--- src/planets/src/se/exuvo/planets/systems/UISystem.java
+++ src/planets/src/se/exuvo/planets/systems/UISystem.java
@@ -2,13 +2,19 @@ package se.exuvo.planets.systems;
 
 import com.artemis.systems.VoidEntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
 
 /**
  * The system responsible for handling user input (keyboard and mouse).
@@ -16,6 +22,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 public class UISystem extends VoidEntitySystem implements InputProcessor {
 
 	private Stage ui;
+	private Window table;
+	private static int debug = 0;
 
 	public UISystem() {
 		ui = createUI();
@@ -40,28 +48,73 @@ public class UISystem extends VoidEntitySystem implements InputProcessor {
 	 * 
 	 * @return the ui
 	 */
-	private static Stage createUI() {
+	private Stage createUI() {
+		int height = Gdx.graphics.getHeight(), width = Gdx.graphics.getWidth();
+
 		Stage stage = new Stage();
 		Skin skin = new Skin(Gdx.files.internal("resources/uiskin.json"));
-		
-		Table table = new Table();
-		table.setFillParent(true);
+
+		 table = new Window("magic here", skin);
+		table.align(Align.center | Align.top);
+		table.setSize(200, height);
+		table.setPosition(-width / 2, -height / 2);
 		stage.addActor(table);
 
-		TextButton button = new TextButton("Click me!", skin);
-		button.setX(0);
-		button.setY(0);
-		button.addListener(new EventListener() {
-			@Override
-			public boolean handle(Event event) {
-				// TODO Auto-generated method stub
-				System.out.println("clicked me!");
-				return false;
-			}
-		});
-		table.addActor(button);
+		// Massa hastighet xy accel xy volym
+
+		addField("Mass", table, skin);
+		addField("Size", table, skin);
+		addField2("Velocity", table, skin);
+		addField2("Acceleration", table, skin);
+
+//		TextButton button = new TextButton("Click me!", skin);
+//		button.addListener(new ChangeListener() {
+//			@Override
+//			public void changed(ChangeEvent event, Actor actor) {
+//				// TODO Auto-generated method stub
+//				System.out.println("clicked me! " + actor);
+//			}
+//		});
+//		table.addActor(button);
 
 		return stage;
+	}
+
+	private TextField addField(String name, Table table, Skin skin) {
+		Label label = new Label(name, skin);
+		TextField field = new TextField("", skin);
+		table.add(label);
+		table.row();
+		table.add(field).expandX().fillX();
+		table.row();
+		return field;
+	}
+
+	private TextFields addField2(String name, Table rootTable, Skin skin) {
+		Table table = new Table(skin);
+		rootTable.add(table).expandX().fillX().row();
+//		table.debugWidget();
+
+		Label label = new Label(name, skin);
+		Label labelX = new Label("X", skin);
+		Label labelY = new Label("Y", skin);
+		TextFields fields = new TextFields();
+		fields.a = new TextField("", skin);
+		fields.b = new TextField("", skin);
+
+		table.add(label).colspan(2);
+		table.row();
+		table.add(labelX).padRight(10).padLeft(5);
+		table.add(fields.a).expandX().fillX();
+		table.row();
+		table.add(labelY).padRight(10).padLeft(5);;
+		table.add(fields.b).fillX();
+		table.row();
+		return fields;
+	}
+
+	private static class TextFields {
+		TextField a, b;
 	}
 
 	@Override
@@ -70,11 +123,40 @@ public class UISystem extends VoidEntitySystem implements InputProcessor {
 	}
 
 	// --input--
+	
+	private void d(){
+		switch(debug){
+			case 1:
+				table.debugTable();
+				break;
+			case 2:
+				table.debugCell();
+				break;
+			case 3:
+				table.debugWidget();
+				break;
+			case 4:
+				table.debug();
+				break;
+			default:
+				table.debug(Debug.none);
+		}
+		table.invalidate();
+	}
 
 	@Override
 	public boolean keyDown(int keycode) {
 		if (ui.keyDown(keycode)) {
 			return true;
+		}
+		if(keycode == Input.Keys.A){
+			debug = (debug+1) % 5;
+			d();
+		}
+		if(keycode == Input.Keys.Z){
+			debug--;
+			if(debug < 0) debug = 4;
+			d();
 		}
 		return false;
 	}
