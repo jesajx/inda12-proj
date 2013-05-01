77db1e20e91a7ce6aa5abceb9f4632da75257797
exuvo <exuvo@exuvo.se>
2013-05-01 23:12:54 +0200

    Removed unused code from Init.

--- src/planets/src/se/exuvo/planets/Init.java
+++ src/planets/src/se/exuvo/planets/Init.java
@@ -18,6 +18,7 @@ import com.martiansoftware.jsap.Switch;
 
 /**
  * Class responsible for the main-method which starts the game.
+ * 
  * @see Planets
  */
 public class Init {
@@ -25,57 +26,50 @@ public class Init {
 	public static final long serialVersionUID = 1;;
 	public static JSAPResult config = null;
 	public static ShutDownHook shutdownhook = new ShutDownHook();
-	
-	
+
 	/**
 	 * @param args
-	 * @return 	1=Invalid Params,
-	 * 			2=Invalid Config,
+	 * @return 1=Invalid Params, 2=Invalid Config,
 	 */
 	public static void main(String[] args) {
 		DOMConfigurator.configure("log4j.xml");
 		log.fatal("### Starting ###");
-		
+
 		JSAP jsap = new JSAP();
-        arguments(jsap);
-        
-        config = jsap.parse(args);
-        // check whether the command line was valid, and if it wasn't, display usage information and exit.
-        if (!config.success()) {
-            System.err.println();
-         // print out specific error messages describing the problems
-            // with the command line, THEN print usage, THEN print full
-            // help.  This is called "beating the user with a clue stick."
-            for (Iterator<?> errs = config.getErrorMessageIterator();
-                    errs.hasNext();) {
-                System.err.println("Error: " + errs.next());
-            }
-            
-            System.err.println();
-            System.err.println("Usage: java "
-                                + Init.class.getName());
-            System.err.println("                "
-                                + jsap.getUsage());
-            System.err.println("All parameters override config settings");
-            System.err.println();
-            // show full help as well
-            System.err.println(jsap.getHelp());
-            System.exit(1);
-        }
-        
-        new Settings(config);
-        Runtime.getRuntime().addShutdownHook(shutdownhook);
-        
-        if(config.getBoolean("test")){
-        	//net.java.games.input.test.ControllerReadTest.main(null);
-        	return;
-        }
-        
-        // load images.
-        ImagePacker.run();
-        
-        // create windows and main game-screen (which happens to manage the mainloop)
-        LwjglApplicationConfiguration lwjglApplicationConfiguration = new LwjglApplicationConfiguration();
+		arguments(jsap);
+
+		config = jsap.parse(args);
+		// check whether the command line was valid, and if it wasn't, display usage information and exit.
+		if (!config.success()) {
+			System.err.println();
+			// print out specific error messages describing the problems
+			// with the command line, THEN print usage, THEN print full
+			// help. This is called "beating the user with a clue stick."
+			for (Iterator<?> errs = config.getErrorMessageIterator(); errs.hasNext();) {
+				System.err.println("Error: " + errs.next());
+			}
+
+			System.err.println();
+			System.err.println("Usage: java " + Init.class.getName());
+			System.err.println("                " + jsap.getUsage());
+			System.err.println("All parameters override config settings");
+			System.err.println();
+			// show full help as well
+			System.err.println(jsap.getHelp());
+			System.exit(1);
+		}
+
+		new Settings(config);
+		Runtime.getRuntime().addShutdownHook(shutdownhook);
+
+//        if(config.getBoolean("test")){
+//        }
+
+		// compress images.
+		ImagePacker.run();
+
+		// create windows and main game-screen (which happens to manage the mainloop)
+		LwjglApplicationConfiguration lwjglApplicationConfiguration = new LwjglApplicationConfiguration();
 		lwjglApplicationConfiguration.fullscreen = Settings.getBol("GUI.Fullscreen");
 		lwjglApplicationConfiguration.width = Settings.getInt("GUI.Width");
 		lwjglApplicationConfiguration.height = Settings.getInt("GUI.Height");
@@ -86,52 +80,23 @@ public class Init {
 //		lwjglApplicationConfiguration.useGL20 = true;
 		new LwjglApplication(new Planets(), lwjglApplicationConfiguration);
 	}
-	
-	private static final void arguments(JSAP jsap){
-		// TODO clean? do we use all these options?
-		Switch list = new Switch("list")
-			.setShortFlag('l')
-			.setLongFlag("list");
-		list.setHelp("List controllers.");
-		
-		Switch test = new Switch("test")
-			.setShortFlag('t')
-			.setLongFlag("test");
-		test.setHelp("Open test windows.");
-		
-		FlaggedOption port = new FlaggedOption("port")
-			.setStringParser(JSAP.STRING_PARSER)
-			.setDefault(JSAP.NO_DEFAULT)
-			.setRequired(false)
-			.setShortFlag('p')
-			.setLongFlag("port");
-		port.setHelp("Port to connect to.");
-		
-		FlaggedOption server = new FlaggedOption("server")
-			.setStringParser(JSAP.STRING_PARSER)
-			.setDefault(JSAP.NO_DEFAULT)
-			.setRequired(false)
-			.setShortFlag('s')
-			.setLongFlag("server");
-		server.setHelp("Server to connect to.");
-		
+
+	private static final void arguments(JSAP jsap) {
+		Switch fscreen = new Switch("fullscreen").setShortFlag('f').setLongFlag("fullscreen");
+		fscreen.setHelp("Run in fullscreen.");
+
 		try {
-			jsap.registerParameter(list);
-			jsap.registerParameter(test);
-			jsap.registerParameter(port);
-			jsap.registerParameter(server);
+			jsap.registerParameter(fscreen);
 		} catch (JSAPException e) {
 			log.warn("JSAP: Failed to register parameters due to: " + e);
 		}
 	}
-
 }
 
-class ShutDownHook extends Thread{
-	public ShutDownHook(){}
-	
-	public void run(){
+class ShutDownHook extends Thread {
+	public ShutDownHook() {}
+
+	public void run() {
 		Settings.save();
 	}
 }
-
